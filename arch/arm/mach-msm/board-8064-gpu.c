2ba1749c4ad336794b3618d0c7f3510dc046a844
robcore <robpatershuk@gmail.com>
2016-05-31 11:11:34 -0600

    some minor changes, replacing logger sysfs implementation

--- arch/arm/mach-msm/board-8064-gpu.c
+++ arch/arm/mach-msm/board-8064-gpu.c
@@ -337,10 +337,8 @@ void __init apq8064_init_gpu(void)
 {
 	unsigned int version = socinfo_get_version();
 
-#ifndef CONFIG_GPU_OVERCLOCK
 	if (cpu_is_apq8064ab())
 		kgsl_3d0_pdata.pwrlevel[0].gpu_freq = 450000000;
-#endif
 	if (SOCINFO_VERSION_MAJOR(version) == 2) {
 		kgsl_3d0_pdata.chipid = ADRENO_CHIPID(3, 2, 0, 2);
 	} else {
