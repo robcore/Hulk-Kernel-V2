cc964926d5077f08bc68474b278813911b68f310
robcore <robpatershuk@gmail.com>
2016-06-04 16:50:06 -0600

    kgsl-ensure cpu and not buggy gpu path is used to write iommu registers

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -790,6 +790,9 @@ static void adreno_iommu_setstate(struct kgsl_device *device,
 					unsigned int context_id,
 					uint32_t flags)
 {
+#ifndef CONFIG_KGSL_PER_PROCESS_PAGE_TABLE
+	return kgsl_mmu_device_setstate(&device->mmu, flags);
+#else
 	unsigned int pt_val, reg_pt_val;
 	unsigned int *link = NULL, *cmds;
 	struct adreno_device *adreno_dev = ADRENO_DEVICE(device);
@@ -940,6 +943,7 @@ static void adreno_iommu_setstate(struct kgsl_device *device,
 done:
 	kfree(link);
 	kgsl_context_put(context);
+#endif /* CONFIG_KGSL_PER_PROCESS_PAGE_TABLE */
 }
 
 static void adreno_gpummu_setstate(struct kgsl_device *device,
@@ -2366,7 +2370,7 @@ _adreno_ft_restart_device(struct kgsl_device *device,
 		KGSL_FT_ERR(device, "Device stop failed\n");
 		return 1;
 	}
-	
+
 	if (adreno_init(device)) {
 		KGSL_FT_ERR(device, "Device start failed\n");
 		return 1;
