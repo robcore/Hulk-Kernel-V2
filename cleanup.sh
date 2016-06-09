101af802a693422e521169802e1fbf9b89bd5bb3
robcore <robpatershuk@gmail.com>
2016-06-09 16:44:13 -0600

    removed junk file and unneccesary line from cleaning script

--- cleanup.sh
+++ cleanup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 rm -rf $(pwd)/out;
 rm $(pwd)/arch/arm/boot/dhd.ko;
-rm $(pwd)/arch/arm/boot/frandom.ko;
 rm $(pwd)/arch/arm/boot/boot.img-zImage;
 # clean up leftover junk
 find . -type f \( -iname \*.rej \
